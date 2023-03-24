115275d954e77aff29d83d4cd2cdee0f42818793
Aeasala <esmacdon@gmail.com>
2023-03-24 15:12:48 -0400

    Fix master link
    Make pretty

---
+++ src/main.cpp
@@ -0,0 +1,38 @@
+/************/
+/* Includes */
+#include <iostream>
+#include "Rectangle.h"
+#include "Square.h"
+/************/
+
+/*********/
+/* Types */
+
+/*********/
+
+/********/
+/* Data */
+
+/********/
+
+/***********************/
+/* Function Prototypes */
+
+/***********************/
+
+///////////////////////////////////////////////////////////////////////
+
+/*********************************************************************/
+/*                       Function Definitions                        */
+
+int main(){
+    Rectangle rect(5, 9);
+    Square sq(3);
+    std::cout << "Hello World!" << std::endl;
+    std::cout << std::to_string(sq.getArea()) << std::endl;
+    std::cout << std::to_string(rect.getArea()) << std::endl;
+    
+    return 0;
+}
+
+/*********************************************************************/
