020fe421132581b8812dce23d77b9954cab82ed4
Aeasala <esmacdon@gmail.com>
2023-03-24 15:17:39 -0400

    remove srcfiles

--- src/Rectangle.cpp
+++
@@ -1,9 +0,0 @@
-#include "Rectangle.h"
-
-/* Create Rectcangle object of length l and width w. */
-Rectangle::Rectangle(int l, int w) : length(l), width(w) {}
-
-/* A=l*w */
-int Rectangle::getArea() {
-    return length * width;
-}
