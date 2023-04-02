cf2af55e65af70b6cb45c6aacc4c89f4cd57fcb8
Aeasala <esmacdon@gmail.com>
2023-04-01 21:03:37 -0400

    bulk-compiles everything.  not selective.

--- src/subdir.mk
+++ src/subdir.mk
@@ -1,17 +1,27 @@
-mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
-here := $(notdir $(patsubst %/,%,$(dir $(mkfile_path))))
-
-#TODO
-
+##mkfile_path := $(abspath $(lastword $(MAKEFILE_LIST)))
 #folder names
-SUBMODULES += asdf meme mame
-SUBMODULES_DIRS += $(patsubst %,$(here)/%/,$(SUBMODULES))
-SUBMODULES_MAKES += $(patsubst %,%/subdir.mk,$(SUBMODULES_DIRS))
+SUBMODULES_DIRS :=${shell find ${SRC}/* -type d -print}
+SUBOBJECTS_DIRS :=$(subst $(SRC),$(OBJ),$(SUBMODULES_DIRS))
 
-OBJ_SUB := $(subst $(SRC),$(OBJ),$(SUBMODULES_DIRS))
+#get stuff by extension
+SUBSOURCES_C:=$(shell find $(SRC)/*/ -name '*.c')
+SUBSOURCES_CC:=$(shell find $(SRC)/*/ -name '*.cc')
+SUBSOURCES_CPP:=$(shell find $(SRC)/*/ -name '*.cpp')
+SUBSOURCES_CXX:=$(shell find $(SRC)/*/ -name '*.cxx')
 
-SUBSOURCES += $(wildcard $(here)/*.c $(here)/*.cc $(here)/*.cpp $(here)/*.cxx)
-SUBOBJECTS := $(patsubst $(SRC)/%.c,$(OBJ)/%.o,$(SUBSOURCES))
-	
-$(info $(OBJ_SUB))
-$(info $(SUBMODULES_DIRS))
+#subobject extension setting, directory location
+SUBOBJECTS_EXT :=\
+	$(SUBSOURCES_C:.c=.o)\
+	$(SUBSOURCES_CC:.cc=.o)\
+	$(SUBSOURCES_CPP:.cpp=.o)\
+	$(SUBSOURCES_CXX:.cxx=.o)
+SUBOBJECTS := $(subst $(SRC),$(OBJ),$(SUBOBJECTS_EXT))
+
+# make a nice tidy list of our subsources that we'll attach on our root makefile source list
+SUBSOURCES = $(SUBSOURCES_C) \
+	$(SUBSOURCES_CC) \
+	$(SUBSOURCES_CPP) \
+	$(SUBSOURCES_CXX) \
+
+#make object directory structure
+$(shell mkdir -p $(SUBOBJECTS_DIRS))
